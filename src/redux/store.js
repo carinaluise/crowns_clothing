6b49aefa82741b0b44df587466d9824c2e5e1750
Carina <carinalrobinson@gmail.com>
2020-08-06 14:10:24 +0200

    removed redux logger from published version

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,13 @@ import {persistStore}from 'redux-persist';
 
 
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development'){
+    middlewares.push(logger);
+}
+
+
 
 export const store = createStore (rootReducer, applyMiddleware(...middlewares))
 
