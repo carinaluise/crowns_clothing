8fecb64e28a82a679b57930128125162052c3116
Carina <carinalrobinson@gmail.com>
2020-08-19 17:57:08 +0200

    async function start amended plus container pattern added

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-import CollectionPage from '../collection/collection';
+// import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+// import CollectionPage from '../collection/collection';
 import {connect} from 'react-redux';
 import {fetchCollectionsStartAsync} from '../../redux/shop/shop.actions';
-import withSpinner from '../../components/with-spinner/with-spinner.component';
-import { selectIsCollectionFetching, selectIsCollectionsLoaded} from '../../redux/shop/shop.selector';
-import {createStructuredSelector} from 'reselect';
+// import withSpinner from '../../components/with-spinner/with-spinner.component';
 
-const CollectionsOverviewWithSpinner = withSpinner(CollectionsOverview);
-const CollectionPageWithSpinner = withSpinner(CollectionPage);
+import CollectionPageContainer from '../collection/collection.container';
+import CollectionsOverviewContainer from '../../components/collections-overview/collection-overview.container'
+// const CollectionsOverviewWithSpinner = withSpinner(CollectionsOverview);
+// const CollectionPageWithSpinner = withSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
  
@@ -26,17 +26,17 @@ fetchCollectionsStartAsync();
 
 render(){
 
-    const {match, isCollectionFetching, isCollectionsLoaded} = this.props
+    const {match} = this.props
 
     return ( 
     <div className="shop-page">
         <Route 
             exact path={`${match.path}`} 
-            render={(props) => <CollectionsOverviewWithSpinner isLoading={isCollectionFetching} {...props}/> }
+            component={CollectionsOverviewContainer} 
         ></Route>
         <Route 
             path={`${match.path}/:collectionId`} 
-            render={(props) => <CollectionPageWithSpinner isLoading={!isCollectionsLoaded} {...props} /> }
+            component={CollectionPageContainer} 
         ></Route>
     </div>
     )
@@ -47,10 +47,6 @@ render(){
 // });
 
 
-const mapStateToProps = createStructuredSelector({
-    isCollectionFetching: selectIsCollectionFetching,
-    isCollectionsLoaded: selectIsCollectionsLoaded
-});
 
 
 const mapDispatchToProps = dispatch => ({
@@ -59,4 +55,4 @@ const mapDispatchToProps = dispatch => ({
 
 });
     
-export default connect(mapStateToProps, mapDispatchToProps)(ShopPage);    
+export default connect(null, mapDispatchToProps)(ShopPage);    
--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,7 +1,6 @@
 import ShopActionTypes from './shop.types';
 import {firestore, convertCollectionsSnapshotToMap} from '../../firebase/firebase.utils';
 
-
 export const updateCollections = (collectionsMap) => ({
 
 type: ShopActionTypes.UPDATE_COLLECTIONS,
@@ -19,15 +18,17 @@ export const fetchCollectionsStartAsync = () => {
 
         dispatch(fetchCollectionsStart());
 
-        collectionRef.get().then(snapshot => {
+        collectionRef
+        .get()
+        .then(snapshot => {
             const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-  
-            dispatch(fetchCollectionSuccess(collectionsMap))
-            .catch(error => dispatch(fetchCollectionsFailure(error)))
-        })      
+            dispatch(fetchCollectionSuccess(collectionsMap)) })
+
+        .catch(error => dispatch(fetchCollectionsFailure(error)))
+        } 
     }
 
-}
+
 
 export const fetchCollectionSuccess = (collectionsMap) => ({
     type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
