2829d83f705229e189b6e362a00f9b39e92bce8e
Carina <carinalrobinson@gmail.com>
2020-09-22 11:47:28 +0200

    implemented clear-cart saga

--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -1,10 +1,11 @@
 
 import {all , call} from 'redux-saga/effects'
 import {userSagas} from './user/user.saga';
+import {cartSagas} from './cart/cart.saga';
 import {fetchCollectionsStart } from '../redux/shop/shop.saga';
 
 export default function* rootSaga() {
 
-	yield all ([ call (fetchCollectionsStart), call(userSagas)]);
+	yield all ([ call (fetchCollectionsStart), call(userSagas), call(cartSagas)]);
 }
 
